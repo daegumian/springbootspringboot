d363a8c923ea4cc174e8c66e8feef77d171a5369
박동훈 <daegumian91@gmail.com>
2023-08-02 23:09:47 +0900

    230802-h

--- BootMywebPractice/src/main/java/com/coding404/myweb/service/TopicMapper.java
+++ BootMywebPractice/src/main/java/com/coding404/myweb/service/TopicMapper.java
@@ -1,5 +1,7 @@
 package com.coding404.myweb.service;
 
+import java.util.ArrayList;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.coding404.myweb.command.TopicVO;
@@ -8,5 +10,6 @@ import com.coding404.myweb.command.TopicVO;
 public interface TopicMapper {
 	
 	public int topicReg(TopicVO vo); //등록
-
+	public ArrayList<TopicVO> getList(); //조회
+	
 }
--- BootMywebPractice/src/main/java/com/coding404/myweb/service/TopicService.java
+++ BootMywebPractice/src/main/java/com/coding404/myweb/service/TopicService.java
@@ -1,9 +1,12 @@
 package com.coding404.myweb.service;
 
+import java.util.ArrayList;
+
 import com.coding404.myweb.command.TopicVO;
 
 public interface TopicService {
 	
 	public int topicReg(TopicVO vo); //등록
+	public ArrayList<TopicVO> getList(); //조회
 
 }
