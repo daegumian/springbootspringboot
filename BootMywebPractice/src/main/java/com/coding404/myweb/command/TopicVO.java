3a8cf240625a8d3611ff3875948a8cfaece4990f
박동훈 <daegumian91@gmail.com>
2023-08-02 21:21:07 +0900

    230802-h

--- BootMywebPractice/src/main/java/com/coding404/myweb/command/TopicVO.java
+++ BootMywebPractice/src/main/java/com/coding404/myweb/command/TopicVO.java
@@ -1,20 +1,18 @@
 package com.coding404.myweb.command;
 
-import java.time.LocalDateTime;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 public class TopicVO {
 	
 	private Integer top_num;
-	private LocalDateTime top_regdate;
+	private String top_regdate;
 	private String top_id;
 	private String top_title;
 	private String top_content;
